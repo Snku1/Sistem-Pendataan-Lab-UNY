2e0eeb0657cd55e318b57f0a8566b632878b88c1
arkham55 <154397924+arkham55@users.noreply.github.com>
2026-05-17 19:21:16 +0700

    update settingseeder

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -9,14 +9,13 @@ class SettingSeeder extends Seeder
 {
     public function run()
     {
+        // Informasi Laboratorium
         Setting::set('lab_name', 'Laboratorium AV & TV');
         Setting::set('lab_address', 'Gedung IDB Lantai 2, FT UNY');
         Setting::set('lab_phone', '(0274) 123456');
-        Setting::set('lab_email', 'lab@uny.ac.id');
-        Setting::set('lab_logo', null);
+
+        // Pengaturan Notifikasi
         Setting::set('notification_enabled', '1');
         Setting::set('notification_days_before', '2');
-        Setting::set('notification_sender_email', 'noreply@labavtv.com');
-        Setting::set('notification_sender_name', 'Sistem Lab AV & TV');
     }
 }
